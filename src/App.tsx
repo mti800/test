509feeb1d1e3f5acb899e9b2d92ae774e93185b3
Matias Gutierrez <matias.gutierrez@uap.edu.ar>
2023-06-01 00:23:37 -0300

    anda pero borre list del 2do param de useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Timer from './components/Timer'
 import { format } from 'date-fns'
 
 export default function App() {
-  const date = format(new Date(), "'Today is a' eeee")
+  const date = format(new Date(), "'Today is' eeee")
   return (
     <main className='flex flex-col w-screen justify-center items-center'>
       <h1 className='text-red-600 text-2xl font-bold'>This should be red. {date}</h1>
--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -15,6 +15,8 @@ export default function CharacterList() {
       .then((data) => setList([...list, ...data.results]));
   }, [page]);
 
+  //useMemo o useCallback?
+  
   const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
@@ -34,3 +36,43 @@ export default function CharacterList() {
     </div>
   );
 }
+
+
+// import { useEffect, useState } from "react";
+
+// interface Person {
+//   name: string;
+//   height: number;
+// }
+
+// export default function CharacterList() {
+//   const [list, setList] = useState<Person[]>([]);
+//   const [page, setPage] = useState(1);
+
+  
+//   useEffect(() => {
+//     fetch(`https://swapi.dev/api/people/?page=${page}`)
+//       .then((res) => res.json())
+//       .then((data) => setList(list.concat(data.results)));
+//   }, [list, page]); 
+
+//   const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//   return (
+//     <div>
+//       <div>
+//         {pages.map((p) => (
+//           <button key={p}onClick={() => setPage(p)} >
+//             {p}
+//           </button>
+//         ))}
+//       </div>
+//       <ul>
+//         {list.map((person) => (
+//           <li>{person.name} - {person.height}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
